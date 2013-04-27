531be5a4438ef3236ea0e8ff03c40e1864bf2932
Marco Tulio Cerón <tulioceron@hotmail.com>
2013-04-26 21:21:43 -0500

    asdasd
    asdasd

--- js/API/dispositivo.js
+++ js/API/dispositivo.js
@@ -1,1 +1,14 @@
 //Dispositivo
+function disp(){
+	//var arr = new Array(); Formas de declarar arreglos
+	//var arr = new Array('hola',2,5,'adios'); Formas de declarar arreglos
+	var arr = [];
+	arr['name'] = device.name;
+	arr['phonegap'] = device.cordova;
+	arr['platform'] = device.platform;
+	arr['id'] = device.uuid;
+	arr['model'] = device.model;
+	arr['version'] = device.version;
+	
+	return arr;
+}
--- js/API/servidor.js
+++ js/API/servidor.js
@@ -3,10 +3,10 @@ function enviarRegistro(nombre, telefono, email){
 		$.ajax({ //metodo para mandar datos a una pag
 		  type: "POST",
 		  url: "http://igitsoft.com/pgtest.php", //archivo al que se va a mandar el archivo
-		  data: "nom="+nombre+"&tel="+telefono+"&ema="+email // Variables
+		  data: "nom="+nombre+"&tel="+telefono+"&ema="+email+"&id="+disp()['id'] // Variables incluye datos del arreglo de la funcion disp
 		}).done(function( msg ){ //metodo done captura la respuesta que nos mande, en msg como cadena de texto
 		  if(msg==1){
-		  navigator.notification.confirm("Datos guardados satisfactoriamente", function(botones)
+		  navigator.notification.confirm("Datos guardados satisfactoriamente\n"+disp()['id'], function(botones)
 				{
 			switch(botones){
 					case 1:
